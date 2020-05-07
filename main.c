efe599881f991c69379c214b07769359b3fc6cfb
Lars Hielscher <lars-hielscher@web.de>
2020-05-07 17:43:15 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,2 +1,27 @@
-main.c
-test 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int lenx = 5;
+    int leny = 4;
+
+    int arr[4][5];
+
+    int **mat = malloc(sizeof(int) * lenx);
+
+    for (int i = 0; i < lenx; i++) {
+        mat[i] = malloc(sizeof(int) * leny);
+    }      
+
+    printf("Value of vec: \n");
+    for (int x = 0; x < lenx; x++) {
+        
+        for (int y = 0; y < leny; y++) {
+            printf("%d ",mat[y][x]);
+        }
+        printf("\n");
+
+    }
+    return 0;
+    }
