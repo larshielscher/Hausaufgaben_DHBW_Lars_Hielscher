80c2b558622c02f02b21a5fbde3d498befff893f
Lars Hielscher <lars-hielscher@web.de>
2020-05-14 20:27:57 +0200

    Create mathFunct.h

---
+++ HA-2/Complex/mathFunct.h
@@ -0,0 +1,10 @@
+#ifndef MATHFUNCT_H_
+#define MATHFUNCT_H_   
+
+double multi(double, double, double, double, double *, double *);
+double div(double, double, double, double, double *, double *);
+double add(double, double, double, double, double *, double *);
+double sub(double, double, double, double, double *, double *);
+
+
+#endif
